cbd3bc5ba31a527ff49bf2b515b4da470cd27a97
Sabareesh Kappagantu <skappa@Sabareeshs-MacBook-Pro.local>
2020-09-30 22:04:53 +0530

    updated code for Add Two Numbers to be more concise

--- LinkedLists/AddTwoNumbers/AddTwoNumbers.js
+++ LinkedLists/AddTwoNumbers/AddTwoNumbers.js
@@ -20,83 +20,66 @@ Explanation: 342 + 465 = 807.
 
 
 /**
- * Definition for singly-linked list.*/
- function ListNode(val, next) {
-     this.val = (val===undefined ? 0 : val)
-     this.next = (next===undefined ? null : next)
- }
- 
-/*
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
  * @param {ListNode} l1
  * @param {ListNode} l2
  * @return {ListNode}
  */
-
 const addTwoNumbers = function(l1, l2) {
-    let current1 = l1;
-    let current2 = l2;
-    let carryOver = 0;
-    let prevNode = null;
-    let head = null;
     
-    console.log("List 1 is : " + current1);
-    console.log("List 2 is : " + current2);
+    let carryOver = 0, sum = 0;
+    let sumPointer = null, head = null;
     
-    if(current1 === null && current2 != null) {
-        return current2;
-    }
-    
-    if(current1 != null && current2 === null) {
-        return current1;
-    }
-    
-    while(current1 != null && current2 != null){
+    while(l1 !== null && l2 !== null){
         
-        let newNode = new ListNode();
-        if(head === null) head = newNode;
-        newNode.val = (current1.val + current2.val + carryOver)%10;
-        carryOver = Math.floor((current1.val + current2.val + carryOver)/10);
-        console.log("CarryOver: " + carryOver);
-        if(!prevNode){
-            prevNode = newNode;
+        sum = Math.floor((l1.val + l2.val + carryOver) % 10);
+        carryOver = Math.floor((l1.val + l2.val + carryOver) / 10);
+        
+        if(sumPointer === null) {
+            sumPointer = new ListNode(sum);
+            head = sumPointer;    
         } else {
-           prevNode.next = newNode;
-           prevNode = newNode; 
+            sumPointer.next = new ListNode(sum);
+            sumPointer = sumPointer.next;
         }
         
-        
-        current1 = current1.next;
-        current2 = current2.next;
-        
+        l1 = l1.next;
+        l2 = l2.next;
     }
     
-    console.log(prevNode);
-    
-    let current = null;
-    if(current1) {
-        current = current1;
-    } else if(current2) {
-        current = current2;
-    }
-    
-    while(current) {
-        let newNode1 = new ListNode();
-        newNode1.val = (current.val + carryOver)%10;
-        carryOver = Math.floor((current.val + carryOver)/10);
-        prevNode.next = newNode1;
-        prevNode = newNode1;
+    while(l1 !== null){
+        sum = Math.floor((l1.val + carryOver) % 10);
+        carryOver = Math.floor((l1.val + carryOver) / 10);
         
-        current = current.next;
+        sumPointer.next = new ListNode(sum);
+        sumPointer = sumPointer.next;
+        
+        l1 = l1.next;
     }
     
-    if(carryOver){
-        let carryNode = new ListNode();
-        carryNode.val = carryOver;
-        prevNode.next = carryNode;
-        prevNode = carryNode;
+    while(l2 !== null){
+        sum = Math.floor((l2.val + carryOver) % 10);
+        carryOver = Math.floor((l2.val + carryOver) / 10);
+        
+        sumPointer.next = new ListNode(sum);
+        sumPointer = sumPointer.next;
+        
+        l2 = l2.next;
+    }
+    
+    if(carryOver !== 0){
+        sumPointer.next = new ListNode(carryOver);
+        sumPointer = sumPointer.next;
     }
     
     return head;
+    
 };
 
 module.exports = addTwoNumbers;
