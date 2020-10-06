73831ac3b2c3961cedd99db3e4366b201f1bfca0
Sabareesh Kappagantu <sabareesh.kappagantu@gmail.com>
2020-10-06 13:01:43 +0530

    Added O1 Space complexity execution for merging

--- LinkedLists/MergeTwoSortedLists/index.js
+++ LinkedLists/MergeTwoSortedLists/index.js
@@ -42,4 +42,27 @@ const mergeTwoLists = function(l1, l2) {
     }
 };
 
+
+const mergeTwoLists_O1Space = function(l1, l2) {
+
+    let dummyHead = {next: null};
+    let tail = dummyHead;
+    
+    while(l1 && l2){
+        if(l1.val < l2.val){
+            tail.next = l1;
+            tail = tail.next;
+            l1 = l1.next;
+        } else {
+            tail.next = l2;
+            tail = tail.next;
+            l2 = l2.next;
+        }
+    }
+    
+    tail.next = l1 || l2;
+    
+    return dummyHead.next;
+}
+
 module.exports = mergeTwoLists;
