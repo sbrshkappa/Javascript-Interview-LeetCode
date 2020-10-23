0db46bc9b337899e700ca5d895f375fdc586c046
Sabareesh Kappagantu <sabareesh.kappagantu@gmail.com>
2020-10-23 11:35:14 +0530

    Added test cases for Binary Tree Max Path Sum

--- Trees and Graphs/BinaryTreeMaxPathSum/test.js
+++ Trees and Graphs/BinaryTreeMaxPathSum/test.js
@@ -1,1 +1,27 @@
-//No Test Case
+const { TestScheduler } = require("jest");
+const maxPathSum = require("./index");
+
+function TreeNode(val, left, right) {
+  this.val = val === undefined ? 0 : val;
+  this.left = left === undefined ? null : left;
+  this.right = right === undefined ? null : right;
+}
+
+test("Valid tree with a max path sum including root", () => {
+  let root = new TreeNode(
+    1,
+    new TreeNode(2, null, null),
+    new TreeNode(3, null, null)
+  );
+  expect(maxPathSum(root)).toEqual(6);
+});
+
+test("Valid tree with a max path sum as subtree", () => {
+  let root = new TreeNode(-10, new TreeNode(9, null, null), null);
+  root.right = new TreeNode(
+    20,
+    new TreeNode(15, null, null),
+    new TreeNode(7, null, null)
+  );
+  expect(maxPathSum(root)).toEqual(42);
+});
